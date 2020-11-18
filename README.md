a6adb6a26947af6dc5cd7325a958c59de152a6a1
Peter Yoachim <yoachim@uw.edu>
2020-11-18 15:01:26 -0800

    slurming

--- README.md
+++ README.md
@@ -6,4 +6,42 @@ Even more simulated Rubin Observatory surveys
 
 # Notes
 
-We are shifting to default to 2x15s visits rather than 1x30s as in previous releases. 
+* Default to 2x15s visits rather than 1x30s as in previous releases. This results in a significant decrease in the total number of visits compared to earlier releases.
+* Updated the telescope kinematic model. Previous releases had bugs that let observations be taken below the altitude limit, as well as unphysical combinations of telescope rotator angles.
+* Improved rolling candence with scalable rolling strength
+
+
+
+# Run Directories
+
+## baseline17
+
+Our latest baseline run. 
+
+## rolling
+
+The latest rolling cadence simulations with the classic footprint
+
+## rolling_nm
+
+Same as rolling, only without modulating nightly North/South emphasis.
+
+## euclid_dither
+
+Use a unique dither pattern for the Euclid DDF pointing to better match the fooprint of the Euclid field.
+
+## pair_times
+
+Vary the amount of time we try to take visit pairs on.
+
+## twi_pairs
+
+Takes twilight observations as pairs. Run with and without trying to re-observe areas in twilight that have already been observed in the night.
+
+## u_long
+
+Take u-band observations as single snaps, and test increasing u-band exposure times. Note, DDF u-band still needs to be updated.
+
+## wfd_cadence_drive
+
+An experiment where long gaps in g-band exposures are avoided, even if that means observing g in bright time.
---
+++ slurm/monster.sh
@@ -0,0 +1,22 @@
+python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/baseline17/baseline.py --nexp 1 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/baseline17/
+python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/baseline17/baseline.py --nexp 2 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/baseline17/
+python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/euclid_dither/euclid_dither.py  --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/euclid_dither/
+python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/pair_times/pair_times.py --pair_time 11 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/pair_times
+python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/pair_times/pair_times.py --pair_time 22 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/pair_times
+python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/pair_times/pair_times.py --pair_time 33 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/pair_times
+python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/pair_times/pair_times.py --pair_time 44 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/pair_times
+python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/pair_times/pair_times.py --pair_time 55 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/pair_times
+python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/twi_pairs/twi_pairs.py --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/twi_pairs
+python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/twi_pairs/twi_pairs.py --repeat_night --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/twi_pairs
+python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/twi_pairs/twi_pairs_mixed.py --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/twi_pairs
+python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/twi_pairs/twi_pairs_mixed.py --repeat_night --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/twi_pairs
+python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/u_long/u_long_msnaps.py --u_expt 30 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/u_long/
+python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/u_long/u_long_msnaps.py --u_expt 40 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/u_long/
+python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/u_long/u_long_msnaps.py --u_expt 50 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/u_long/
+python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/u_long/u_long_msnaps.py --u_expt 60 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/u_long/
+python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive/cadence_drive.py --gfill_limit 30 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive
+python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive/cadence_drive.py --gfill_limit 100 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive
+python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive/cadence_drive.py --gfill_limit 200 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive
+python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive/cadence_drive.py --gfill_limit 30 --gcb --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive
+python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive/cadence_drive.py --gfill_limit 100 --gcb --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive
+python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive/cadence_drive.py --gfill_limit 200 --gcb --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive
