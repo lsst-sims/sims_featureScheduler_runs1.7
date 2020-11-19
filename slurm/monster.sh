6a7061425231cfce5806e37ad7677fb70fc2396e
Peter Yoachim <yoachim@uw.edu>
2020-11-19 08:40:36 -0800

    monster on scratch

--- slurm/monster.sh
+++ slurm/monster.sh
@@ -1,20 +1,46 @@
-python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/euclid_dither/euclid_dither.py  --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/euclid_dither/
-python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/pair_times/pair_times.py --pair_time 11 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/pair_times
-python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/pair_times/pair_times.py --pair_time 22 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/pair_times
-python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/pair_times/pair_times.py --pair_time 33 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/pair_times
-python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/pair_times/pair_times.py --pair_time 44 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/pair_times
-python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/pair_times/pair_times.py --pair_time 55 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/pair_times
-python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/twi_pairs/twi_pairs.py --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/twi_pairs
-python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/twi_pairs/twi_pairs.py --repeat_night --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/twi_pairs
-python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/twi_pairs/twi_pairs_mixed.py --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/twi_pairs
-python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/twi_pairs/twi_pairs_mixed.py --repeat_night --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/twi_pairs
-python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/u_long/u_long_msnaps.py --u_expt 30 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/u_long/
-python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/u_long/u_long_msnaps.py --u_expt 40 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/u_long/
-python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/u_long/u_long_msnaps.py --u_expt 50 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/u_long/
-python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/u_long/u_long_msnaps.py --u_expt 60 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/u_long/
-python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive/cadence_drive.py --gfill_limit 30 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive
-python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive/cadence_drive.py --gfill_limit 100 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive
-python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive/cadence_drive.py --gfill_limit 200 --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive
-python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive/cadence_drive.py --gfill_limit 30 --gcb --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive
-python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive/cadence_drive.py --gfill_limit 100 --gcb --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive
-python /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive/cadence_drive.py --gfill_limit 200 --gcb --outDir /gscratch/astro/yoachim/git_repos/sims_featureScheduler_runs1.7/wfd_cadence_drive
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/baseline/baseline.py --nexp 1 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/baseline17/
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/baseline/baseline.py --nexp 2 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/baseline17/
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/euclid_dither/euclid_dither.py  --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/euclid_dither/
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/pair_times/pair_times.py --pair_time 11 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/pair_times
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/pair_times/pair_times.py --pair_time 22 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/pair_times
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/pair_times/pair_times.py --pair_time 33 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/pair_times
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/pair_times/pair_times.py --pair_time 44 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/pair_times
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/pair_times/pair_times.py --pair_time 55 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/pair_times
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling/rolling.py --nslice 2 --scale 0.2 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling/rolling.py --nslice 2 --scale 0.4 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling/rolling.py --nslice 2 --scale 0.6 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling/rolling.py --nslice 2 --scale 0.8 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling/rolling.py --nslice 2 --scale 0.9 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling/rolling.py --nslice 2 --scale 1   --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling/rolling.py --nslice 3 --scale 0.2 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling/rolling.py --nslice 3 --scale 0.4 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling/rolling.py --nslice 3 --scale 0.6 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling/rolling.py --nslice 3 --scale 0.8 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling/rolling.py --nslice 3 --scale 0.9 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling/rolling.py --nslice 3 --scale 1   --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm/rolling_nm.py --nslice 2 --scale 0.2 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm/rolling_nm.py --nslice 2 --scale 0.4 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm/rolling_nm.py --nslice 2 --scale 0.6 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm/rolling_nm.py --nslice 2 --scale 0.8 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm/rolling_nm.py --nslice 2 --scale 0.9 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm/rolling_nm.py --nslice 2 --scale 1   --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm/rolling_nm.py --nslice 3 --scale 0.2 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm/rolling_nm.py --nslice 3 --scale 0.4 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm/rolling_nm.py --nslice 3 --scale 0.6 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm/rolling_nm.py --nslice 3 --scale 0.8 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm/rolling_nm.py --nslice 3 --scale 0.9 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm/rolling_nm.py --nslice 3 --scale 1   --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/rolling_nm
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/twi_pairs/twi_pairs.py --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/twi_pairs
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/twi_pairs/twi_pairs.py --repeat_night --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/twi_pairs
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/twi_pairs/twi_pairs_mixed.py --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/twi_pairs
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/twi_pairs/twi_pairs_mixed.py --repeat_night --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/twi_pairs
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/u_long/u_long_msnaps.py --u_expt 30 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/u_long/
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/u_long/u_long_msnaps.py --u_expt 40 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/u_long/
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/u_long/u_long_msnaps.py --u_expt 50 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/u_long/
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/u_long/u_long_msnaps.py --u_expt 60 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/u_long/
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/wfd_cadence_drive/cadence_drive.py --gfill_limit 30 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/wfd_cadence_drive
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/wfd_cadence_drive/cadence_drive.py --gfill_limit 100 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/wfd_cadence_drive
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/wfd_cadence_drive/cadence_drive.py --gfill_limit 200 --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/wfd_cadence_drive
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/wfd_cadence_drive/cadence_drive.py --gfill_limit 30 --gcb --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/wfd_cadence_drive
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/wfd_cadence_drive/cadence_drive.py --gfill_limit 100 --gcb --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/wfd_cadence_drive
+python /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/wfd_cadence_drive/cadence_drive.py --gfill_limit 200 --gcb --outDir /gscratch/scrubbed/yoachim/sims_featureScheduler_runs1.7/wfd_cadence_drive
