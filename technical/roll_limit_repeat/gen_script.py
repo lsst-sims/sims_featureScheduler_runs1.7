8b18682b026ea71b33eefa3685c428f6237ec024
Peter Yoachim <yoachim@uw.edu>
2021-02-22 13:44:38 -0800

    even more

--- technical/roll_limit_repeat/gen_script.py
+++ technical/roll_limit_repeat/gen_script.py
@@ -1,13 +1,15 @@
 
-weights = [0.75, 0.9]
+scales = [0.75, 0.9]
 slices = [2, 3]
 nrws = [-6., -3., -1., 0.]
+weights = [0.6, 0.9, 1.2]
 
 command = 'python rolling_nm.py '
 with open('run_rolling.script', 'w') as f:
-    for weight in weights:
-        for slice_val in slices:
-            for nrw in nrws:
-                result = command + '--nslice %i --fpw %f --nrw %f' % (slice_val, weight, nrw)
+    for scale in scales:
+        for weight in weights:
+            for slice_val in slices:
+                for nrw in nrws:
+                    result = command + '--nslice %i --fpw %.1f --nrw %.1f --scale %.2f' % (slice_val, weight, nrw, scale)
 
-                print(result, file=f)
+                    print(result, file=f)
